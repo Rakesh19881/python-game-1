b1aa49138c9a222cb3fa574c3c401a8383ca4fa3
Rakesh sahu <rakesh.s.19881@gmail.com>
2025-12-18 06:57:02 +0530

    Add Guess the Number game implementation

---
+++ guess_the_number.py
@@ -0,0 +1,19 @@
+import random
+
+number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to Guess the Number Game!")
+print("I have chosen a number between 1 and 100.")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < number:
+        print("Too low!")
+    elif guess > number:
+        print("Too high!")
+    else:
+        print(f"Congratulations! You guessed it in {attempts} attempts.")
+        break
